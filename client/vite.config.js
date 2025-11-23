c2a561deef5b2505329e08cf659f19593124a5da
Ubiquity89 <ubiquity89@gmail.com>
2025-11-23 19:53:36 +0530

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// client/vite.config.js
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5178,
-    proxy: {
-      '/api': {
-        target: 'https://quikkart-1.onrender.com',  // Update this line
-        changeOrigin: true,
-        secure: true,  // Changed to true for HTTPS
-        rewrite: (path) => path.replace(/^\/api/, '')  // Optional: Remove /api prefix if needed
-      }
-    }
-  }
+  base: '/', // ensures all paths in the build work correctly
 });
